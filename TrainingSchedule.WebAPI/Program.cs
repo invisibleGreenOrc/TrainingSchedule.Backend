0a89b053a198f760d6f6d87b5214c379714d9c34
InvisibleGreenOrc <Chesantp@yandex.ru>
2022-07-30 17:55:33 +0500

    Delete comments.

--- TrainingSchedule.WebAPI/Program.cs
+++ TrainingSchedule.WebAPI/Program.cs
@@ -12,10 +12,7 @@ namespace TrainingSchedule.WebAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
